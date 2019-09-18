preprocessing : normalization -> standardization; ph bug fixing

postprocessing/reshape.py
@@ -11,6 +11,6 @@ def reshape_day(results, hist, ph, freq):
         :param freq: sampling frequency in minutes (e.g., 5)
         :return: reshape results of shape (n_days, None, 2)
     """
-    preds_per_day = day_len // freq - ph // freq - hist // freq
+    preds_per_day = day_len // freq - ph // freq - hist // freq + 1
 
     return np.reshape(results, (-1, preds_per_day, 2))

preprocessing/reshape.py
@@ -26,8 +26,8 @@ def reshape_day(data, ph, freq):
 
     # for every day, compute the objective prediction, based on the prediction horizon
     def compute_y(day, ph):
-        y1 = day.ix[ph - 1:len(day.index) - 1, "glucose"].copy().reset_index(drop=True)
-        y2 = day.ix[ph:, "glucose"].copy().reset_index(drop=True)
+        y1 = day.ix[ph - 1:len(day.index) - 2, "glucose"].copy().reset_index(drop=True)
+        y2 = day.ix[ph:len(day.index) - 1, "glucose"].copy().reset_index(drop=True)
         day_y = day.ix[:len(day.index) - ph - 1].copy().reset_index(drop=True)
         day_y["y_ph-1"], day_y["y_ph"] = y1, y2
         return day_y
@@ -61,15 +61,15 @@ def reshape_samples_with_history(data, hist, freq):
         for day in split:
             # create the samples by adding the past values accounting for the amount of history
             X = np.array([
-                day.ix[j: j + day_len_freq - hist_freq - 1, ["glucose", "insulin", "CHO"]].values for j in
+                day.ix[j: j + day_len_freq - hist_freq, ["glucose", "insulin", "CHO"]].values for j in
                 range(hist_freq)
             ])
             # the for loop is done the other way around to speed up, but we need to transpose after
             X = np.rollaxis(X, 1, 0)
 
             X = np.reshape(X, (X.shape[0], -1))
-            y1 = day.ix[hist_freq:, ["y_ph-1"]].values.reshape(-1, 1)
-            y2 = day.ix[hist_freq:, ["y_ph"]].values.reshape(-1, 1)
+            y1 = day.ix[hist_freq-1:, ["y_ph-1"]].values.reshape(-1, 1)
+            y2 = day.ix[hist_freq-1:, ["y_ph"]].values.reshape(-1, 1)
 
             days.append(pd.DataFrame(data=np.c_[X, y1, y2], columns=columns))
         splits.append(pd.concat(days).reset_index(drop=True))

preprocessing/scaling.py
@@ -1,27 +1,20 @@
-import numpy as np
-
-
-def normalize(data, min=None, max=None):
+def standardize(data, mean=None, std=None):
     """
-        Min-max normalization (scaling of the features between -1 and 1) of the data.
-        :param data: input data of shape (n_splits, n_days, None, 5)
-        :param min: if None, min and max are computed, fitted to the data, else transformation is applied
-        :param max: see min
-        :return: normalized data of shape (n_splits, n_days, None, 5), list of min (one vector per split),
-                list of max (one vector per split)
+        Zero-mean and unit-variance standardization of the data.
+        :param data: input data of shape (n_splits, None, 110)
+        :param mean: if None, mean and std are computed, fitted to the data
+        :param std: see min
+        :return: standardized data of shape (n_splits, None, 110), list of means (one vector per split),
+                list of stds (one vector per split)
     """
     # if min and max are not given, we need to fit the min max scaling
-    if min is None and max is None:
-        min, max = [], []
+    if mean is None and std is None:
+        mean, std = [], []
         for split in data:
-            min_max = np.array([[df.min().values, df.max().values] for df in split])
-            min.append(np.min(min_max[:, 0], axis=0))
-            max.append(np.max(min_max[:, 1], axis=0))
+            mean.append(split.mean().values)
+            std.append(split.std().values)
 
     # transform the data
-    data_norm = [
-        [2 * (day - min_split) / (max_split - min_split) - 1 for day in split]
-        for split, min_split, max_split in zip(data, min, max)
-    ]
+    data_norm = [(split - mean_split) / std_split for split, mean_split, std_split in zip(data, mean, std)]
 
-    return data_norm, min, max
+    return data_norm, mean, std